1295f05263f06f4d026c51b7415adf2e30d8f6dd
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2023-11-28 15:32:38 +1100

    Fixed up the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import './../styles/Footer.css';
+
+import GitHubLogo from './../assets/github.png';
+import LinkedInLogo from './../assets/linkedin.png';
 
 const Footer = () => {
 
-  const containerStyle = {
-    position: 'relative',
-    height: '10vh',
-    backgroundColor: '#14131b',
-    color: 'white',
-    fontSize: '2rem',
-    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
-  };
+  const [shouldRender, setShouldRender] = useState(false);
+
+  useEffect(() => {
+
+    const handleResize = () => {
+      setShouldRender(window.innerWidth > 470);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, []);
 
   return (
-    <div style={containerStyle}>
+    <div className="footer">
+
       <p>Rohan Fredriksson</p>
+
+      <div>
+
+        <a href={'https://www.linkedin.com/in/rohanfredriksson/'}
+          style={{display: 'block', width: '48px', height: '48px', float: 'left', marginRight: '12px'}}
+        >
+          <img
+            style={{position: 'relative', width: '48px', height: '48px'}}
+            src={LinkedInLogo}
+            alt={"LinkedIn"}
+          />
+        </a>
+
+        <a href={'https://github.com/RohanFredriksson'}
+          style={{display: 'block', width: '48px', height: '48px', overflow: 'hidden', borderRadius: '12px'}}
+        >
+          <div
+            className="connection"
+            style={{backgroundColor: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '48px', height: '48px'}}
+          >
+            <img
+              style={{position: 'relative', width: '38px', height: '38px'}}
+              src={GitHubLogo}
+              alt={"GitHub"}
+            />
+          </div>
+        </a>
+
+      </div>
+
+      {shouldRender &&
+        <p>Living Legend</p>
+      }
+
     </div>
   );
 
