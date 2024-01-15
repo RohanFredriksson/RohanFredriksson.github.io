f49a1669323d02c6d5daa0f79f59abea4034dab1
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2024-01-15 23:43:40 +1100

    Added a few more projects with some temporary images, and fixed title styling.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,8 @@ import FractalExploreStatic from './../assets/fractal-explore-static.png';
 import GIFToContributionAnimation from './../assets/gif-to-contribution.gif';
 import GIFToContribtutionStatic from './../assets/gif-to-contribution-static.png';
 import MakeTenStatic from './../assets/make-ten-static.png';
+import SecureMessagingStatic from './../assets/secure-messaging-static.png';
+import RocBotStatic from './../assets/rocbot-static.png';
 
 const scroll = (id) => {
   if (typeof id !== 'string') {return;}
@@ -21,42 +23,31 @@ const scroll = (id) => {
   if (element) {element.scrollIntoView({behavior: 'smooth'});}
 };
 
-const Navigation = () => {
-
-  return (
-    <div className="navigation-container">
-      <ul className="navigation-list">
-        <li><a className="navigation-link" href="https://github.com/RohanFredriksson">Resume</a></li>
-        <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#contact'); scroll('contact');}}>Contact</p></li>
-        <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#projects'); scroll('projects');}}>Projects</p></li>
-        <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#about'); scroll('about');}}>About</p></li>
-      </ul>
-    </div>
-  );
-
-}
-
-const LandingBackground = () => {
-  return <svg preserveAspectRatio="xMidYMid slice" viewBox="10 10 80 80">
-    <path fill="#9b5de5" class="out-top" d="M37-5C25.1-14.7,5.7-19.1-9.2-10-28.5,1.8-32.7,31.1-19.8,49c15.5,21.5,52.6,22,67.2,2.3C59.4,35,53.7,8.5,37-5Z"/>
-    <path fill="#f15bb5" class="in-top" d="M20.6,4.1C11.6,1.5-1.9,2.5-8,11.2-16.3,23.1-8.2,45.6,7.4,50S42.1,38.9,41,24.5C40.2,14.1,29.4,6.6,20.6,4.1Z"/>
-    <path fill="#00bbf9" class="out-bottom" d="M105.9,48.6c-12.4-8.2-29.3-4.8-39.4.8-23.4,12.8-37.7,51.9-19.1,74.1s63.9,15.3,76-5.6c7.6-13.3,1.8-31.1-2.3-43.8C117.6,63.3,114.7,54.3,105.9,48.6Z"/>
-    <path fill="#00f5d4" class="in-bottom" d="M102,67.1c-9.6-6.1-22-3.1-29.5,2-15.4,10.7-19.6,37.5-7.6,47.8s35.9,3.9,44.5-12.5C115.5,92.6,113.9,74.6,102,67.1Z"/>
-  </svg>
-};
-
 const Landing = () => {
 
   return (
     <div style={{position: 'relative', height: '100vh',}}>
       
-      <LandingBackground/>
-      <Navigation/>
-    
-      <div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
-        <p style={{color: 'white', fontSize: '2.5rem', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Hi, my name is<br/>Rohan&nbsp;Fredriksson</p>
+      <svg preserveAspectRatio="xMidYMid slice" viewBox="10 10 80 80">
+        <path fill="#9b5de5" class="out-top" d="M37-5C25.1-14.7,5.7-19.1-9.2-10-28.5,1.8-32.7,31.1-19.8,49c15.5,21.5,52.6,22,67.2,2.3C59.4,35,53.7,8.5,37-5Z"/>
+        <path fill="#f15bb5" class="in-top" d="M20.6,4.1C11.6,1.5-1.9,2.5-8,11.2-16.3,23.1-8.2,45.6,7.4,50S42.1,38.9,41,24.5C40.2,14.1,29.4,6.6,20.6,4.1Z"/>
+        <path fill="#00bbf9" class="out-bottom" d="M105.9,48.6c-12.4-8.2-29.3-4.8-39.4.8-23.4,12.8-37.7,51.9-19.1,74.1s63.9,15.3,76-5.6c7.6-13.3,1.8-31.1-2.3-43.8C117.6,63.3,114.7,54.3,105.9,48.6Z"/>
+        <path fill="#00f5d4" class="in-bottom" d="M102,67.1c-9.6-6.1-22-3.1-29.5,2-15.4,10.7-19.6,37.5-7.6,47.8s35.9,3.9,44.5-12.5C115.5,92.6,113.9,74.6,102,67.1Z"/>
+      </svg>
+
+      <div className="navigation-container">
+        <ul className="navigation-list">
+          <li><a className="navigation-link" href="https://github.com/RohanFredriksson">Resume</a></li>
+          <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#contact'); scroll('contact');}}>Contact</p></li>
+          <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#projects'); scroll('projects');}}>Projects</p></li>
+          <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#about'); scroll('about');}}>About</p></li>
+        </ul>
       </div>
 
+      <div className="title-container">
+        <p className="title">Hi, my name is<br/>Rohan&nbsp;Fredriksson</p>
+      </div>
+      
     </div>
   );
 
@@ -145,13 +136,31 @@ const MakeTen = () => {
   />
 }
 
+const SecureMessaging = () => {
+  return <HoverGif
+    animation={SecureMessagingStatic}
+    frame={SecureMessagingStatic}
+    alt={'Secure Message Webchat'}
+  />
+}
+
+const RocBot = () => {
+  return <HoverGif
+    animation={RocBotStatic}
+    frame={RocBotStatic}
+    alt={'RocBot Discord Music Streaming'}
+  />
+}
+
 const Projects = () => {
 
   const images = [
     { id: 1, title: 'Pancake Engine', href: 'https://github.com/RohanFredriksson/PancakeEngine', contents: (<PancakeEngine/>)},
     { id: 2, title: 'Fractal Explore', href: 'https://github.com/RohanFredriksson/FractalExplore', contents: (<FractalExplore/>)},
     { id: 3, title: 'GIF to Contribution', href: 'https://github.com/RohanFredriksson/GIF-to-Contribution', contents: (<GIFToContribution/>)},
-    { id: 4, title: 'Make Ten Calculator', href: 'https://github.com/RohanFredriksson/MakeTen', contents: <MakeTen/>}
+    { id: 4, title: 'Make Ten Calculator', href: 'https://github.com/RohanFredriksson/MakeTen', contents: <MakeTen/>},
+    { id: 4, title: 'Encrypted Web Chat', href: 'https://github.com/RohanFredriksson/SecureMessagingWebsite', contents: <SecureMessaging/>},
+    { id: 5, title: 'Discord Music Bot', href: 'https://github.com/RohanFredriksson/RocBot', contents: <RocBot/>},
   ];  
 
   return (
