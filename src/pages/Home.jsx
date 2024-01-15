55390dbb497064eaac9c618b24640b9094fae5a9
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2024-01-15 13:46:26 +1100

    Update to the landing background, readded navigation bar.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,28 +24,34 @@ const Navigation = () => {
 
   return (
     <ul className="navigation">
-      <li onClick={() => {window.history.pushState(null, null, '#home'); scroll('home');}}>Home</li>
-      <li onClick={() => {window.history.pushState(null, null, '#about'); scroll('about');}}>About</li>
-      <li onClick={() => {window.history.pushState(null, null, '#projects'); scroll('projects');}}>Projects</li>
+      <li><a className="navigation-link">Resume</a></li>
+      <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#contact'); scroll('contact');}}>Contact</p></li>
+      <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#projects'); scroll('projects');}}>Projects</p></li>
+      <li><p className="navigation-link" onClick={() => {window.history.pushState(null, null, '#about'); scroll('about');}}>About</p></li>
     </ul>
   );
 
 }
 
 const LandingBackground = () => {
-  return <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: '#14131b', zIndex: -10}}></div>;
+  return <svg preserveAspectRatio="xMidYMid slice" viewBox="10 10 80 80">
+    <path fill="#9b5de5" class="out-top" d="M37-5C25.1-14.7,5.7-19.1-9.2-10-28.5,1.8-32.7,31.1-19.8,49c15.5,21.5,52.6,22,67.2,2.3C59.4,35,53.7,8.5,37-5Z"/>
+    <path fill="#f15bb5" class="in-top" d="M20.6,4.1C11.6,1.5-1.9,2.5-8,11.2-16.3,23.1-8.2,45.6,7.4,50S42.1,38.9,41,24.5C40.2,14.1,29.4,6.6,20.6,4.1Z"/>
+    <path fill="#00bbf9" class="out-bottom" d="M105.9,48.6c-12.4-8.2-29.3-4.8-39.4.8-23.4,12.8-37.7,51.9-19.1,74.1s63.9,15.3,76-5.6c7.6-13.3,1.8-31.1-2.3-43.8C117.6,63.3,114.7,54.3,105.9,48.6Z"/>
+    <path fill="#00f5d4" class="in-bottom" d="M102,67.1c-9.6-6.1-22-3.1-29.5,2-15.4,10.7-19.6,37.5-7.6,47.8s35.9,3.9,44.5-12.5C115.5,92.6,113.9,74.6,102,67.1Z"/>
+  </svg>
 };
 
 const Landing = () => {
 
   return (
-    <div style={{position: 'relative', height: '100vh', color: 'white', fontSize: '2rem', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',}}>
-  
+    <div style={{position: 'relative', height: '100vh',}}>
+      
       <LandingBackground/>
+      <Navigation/>
     
-      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-        <h1>Hi, my name is</h1>
-        <h1>Rohan Fredriksson</h1>
+      <div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+        <p style={{color: 'white', fontSize: '2.5rem', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Hi, my name is<br/>Rohan&nbsp;Fredriksson</p>
       </div>
 
     </div>
@@ -164,7 +170,7 @@ const Home = () => {
 
   useEffect(() => {
 
-    scroll(window.location.hash);
+    //scroll(window.location.hash);
 
     const change = () => {
       const next = window.location.hash;
